66daffc131c8d00e13eb22d2651fde0cebadb8d2
Jonas Freire <jonasfreireperson@gmail.com>
2020-12-18 19:56:33 -0300

    First Project .NET - Console Program

---
+++ FirstProjectInCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProjectInCSharp {
+    class Program {
+        static void Main(string[] args) {
+            int number = 9;
+
+            Console.WriteLine($"Tab of {number}:\n");
+            for (int multiple = 1; multiple <= 1000; multiple++) {
+                Console.WriteLine($"{number} x {multiple} = {number * multiple}");
+            }
+        }
+    }
+}

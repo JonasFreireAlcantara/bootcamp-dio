f3ad3e38df5d3d8db89af581977e70bb4469af63
Jonas Freire <jonasfreireperson@gmail.com>
2021-01-09 23:40:23 -0300

    Addition of TodoAPI and curso projects
    TodoAPI:
      Tutorial of Microsoft teaching how to create your Web API with .NET
      5.0
      source: https://docs.microsoft.com/pt-br/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio
    
    curso:
      Curso "Configuração de arquitetura back-end com .NET Core"

---
+++ curso/curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,83 @@
+﻿using curso.api.Filters;
+using curso.api.Infrasctructure.Data;
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        /// <summary>
+        /// Log your account into the system passing the login and senha through the JSON body of post method
+        /// </summary>
+        /// <param name="loginViewModeInput"></param>
+        /// <returns></returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModeInput)
+        {
+            var registrarViewModelInput = new RegistrarViewModelInput()
+            { 
+                Login = "Jonas",
+                Email = "jonas@domain.com",
+                Senha = "teste123"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("MzfsT&d9gprP>!9$Es(X!5g@;ef!5sbk:jH\\2.}8ZP'qY#7");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, registrarViewModelInput.Email.ToString()),
+                    new Claim(ClaimTypes.Name, registrarViewModelInput.Login.ToString()),
+                    new Claim(ClaimTypes.Name, registrarViewModelInput.Senha.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+            return Ok(new
+            { 
+                Token = token,
+                Usuario = registrarViewModelInput
+            });
+        }
+
+        /// <summary>
+        /// Method responsible for registration of new account into the system
+        /// </summary>
+        /// <param name="registrarViewModelInput"></param>
+        /// <returns></returns>
+        [SwaggerResponse(statusCode: 200, description: "Registro realizado", Type = typeof(RegistrarViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro do servidor", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistrarViewModelInput registrarViewModelInput)
+        {
+            var options = new DbContextOptionsBuilder<CursoDBContext>();
+            options.UseSqlServer("Server=localhost");
+
+            CursoDBContext contexto = new CursoDBContext(options);
+
+            return Created("", registrarViewModelInput);
+        }
+    }
+}
